251e52928c0da4346e24e6293f0d2a1778fceb06
Narumi <narumi@Narumis-MBP.mediegruppen.se>
2020-02-19 13:11:32 +0100

    css funkar

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const config = require('./config/config')
 const { app, port } = require('./src/server')
 
+app.set('view engine', 'ejs')
+
+
+
 const dbOptions = {useUnifiedTopology: true, useNewUrlParser: true}
 mongoose.connect(config.databaseURL, dbOptions).then(() => {
     app.listen(port, () => console.log(`Example all listening on ${port}!`))
--- src/server.js
+++ src/server.js
@@ -5,7 +5,12 @@ const port = 8080
 const mongoose = require('mongoose')
 
 //route till servern
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => {
+    const enGrej = "Tjohej"
+    res.status(200).render('gallery', {enGrej})
+})
+//css
+app.use('/public', express.static('public'));
 
 // kicka igång servrern
 module.exports = { app, port }
